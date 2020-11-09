2cbbcc3aa4fa891d960efb91c7e1c87a2bb89d4e
Eduardo EPEC. Perez Cajaraville <eduardo.perezc@stkproj.com>
2020-11-09 17:45:09 +0100

    editCardForm component

---
+++ src/components/EditCardForm/EditCardForm.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import FormModal from '../FormModal/FormModal';
+
+const EditCardForm = ({ closeButtonClick, submitChanges, card }) => {
+    const [form, setForm] = useState(card);
+    const [error, setError] = useState({
+        title: false,
+        description: false,
+        image: false,
+    });
+
+    console.log('form en edit card', form);
+
+    const imageRegexp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+
+    //función que comprueba si el campo está vacío, y de ser así, setea el error
+    const getErrorByEmpty = (value, name) => {
+        if (value === '' || value.trim() === '') {
+            setError({
+                ...error,
+                [name]: true,
+            });
+        } else {
+            setError({
+                ...error,
+                [name]: false,
+            });
+        }
+    };
+    //función que comprueba la validez de la url de la imagen, y de no ser válida, setea error
+    const getErrorUrl = (value, name) => {
+        if (!value.match(imageRegexp)) {
+            setError({
+                ...error,
+                [name]: true,
+            });
+        } else {
+            setError({
+                ...error,
+                [name]: false,
+            });
+        }
+    };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setForm({
+            ...form,
+            [name]: value,
+        });
+
+        //comprobamos si el campo es de image, y si no está vacío
+        if (name === 'image' && value !== '') {
+            getErrorUrl(value, name);
+        }
+        //comprobamos que el campo no sea image (porque si image )
+        else if (name !== 'image') {
+            getErrorByEmpty(value, name);
+        }
+        //última opción, si es campo imagen, y está vacío, seteamos error a false
+        else {
+            setError({
+                ...error,
+                image: false,
+            });
+        }
+    };
+
+    const submitCard = (e) => {
+        e.preventDefault();
+
+        const { title, description } = form;
+
+        if (title.trim() !== '' && description.trim() !== '') {
+            submitChanges(form);
+            closeButtonClick();
+        } else if (title.trim() === '') {
+            setError({
+                ...error,
+                title: true,
+            });
+        } else {
+            setError({
+                ...error,
+                description: true,
+            });
+        }
+    };
+
+    return (
+        <FormModal
+            closeButtonClick={closeButtonClick}
+            formTitle='Modifica tu tarjeta'
+            submitButtonLabel='Guardar'
+            onSubmit={submitCard}
+            formFields={[
+                {
+                    name: 'title',
+                    placeholder: 'Título',
+                    onChange: handleChange,
+                    helpermessage: error.title ? 'El título no puede estar vacío' : '',
+                    value: form.title,
+                },
+                {
+                    name: 'description',
+                    placeholder: 'Descripción',
+                    onChange: handleChange,
+                    helpermessage: error.description ? 'La descripción no puede estar vacía' : '',
+                    value: form.description,
+                },
+                {
+                    name: 'image',
+                    placeholder: 'Imagen (Url)',
+                    onChange: handleChange,
+                    helpermessage: error.image ? 'La url de la imagen no es correcta' : '',
+                    value: form.image,
+                },
+            ]}
+        />
+    );
+};
+
+export default EditCardForm;
