e57ef805324a0e375da494cc0236a5fa9bb49f61
Eduardo EPEC. Perez Cajaraville <eduardo.perezc@stkproj.com>
2020-11-09 17:46:25 +0100

    dashboard mount

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import Card from '../Card/Card';
-import FormModal from '../FormModal/FormModal';
+import AddCardForm from '../AddCardForm/AddCardForm';
 import Header from '../Header/Header';
 import RoundButton from '../Buttons/RoundButton/RoundButton';
+import Orderer from '../Orderer/Orderer';
+import {
+    compareTitleAsc,
+    compareTitleDes,
+    compareDateAsc,
+    compareDateDes,
+} from '../../helpers/helpers';
+import EditCardForm from '../EditCardForm/EditCardForm';
 
 const Dashboard = () => {
-    const [modalVisibility, setModalVisibility] = useState(false);
+    const [addCardFormVisibility, setAddCardFormVisibility] = useState(false);
+    const [editCard, setEditCard] = useState({});
     const [cards, setCards] = useState([]);
+    const [orderBy, setOrderBy] = useState('Ordenar por');
 
     console.log('Cards', cards);
+    console.log('editCard', editCard);
 
-    const viewForm = () => {
-        setModalVisibility(true);
+    const viewAddCard = () => {
+        setAddCardFormVisibility(true);
     };
 
     const addCard = (content) => {
@@ -26,8 +37,57 @@ const Dashboard = () => {
         setCards(newCardsArray);
     };
 
+    const saveCard = (content) => {
+        const { id, title, description, image } = content;
+
+        // const newCardsArray = cards.filter((card) => card.id !== content.id);
+        // newCardsArray.push(content);
+        // setCards(newCardsArray);
+
+        setCards(
+            cards.map((card) => {
+                return card.id === id
+                    ? {
+                          ...card,
+                          title,
+                          description,
+                          image,
+                      }
+                    : card;
+            }),
+        );
+    };
+
+    const viewEditCard = (card) => {
+        console.log('card', card);
+        setEditCard(card);
+    };
+
+    const setOrderCards = (order) => {
+        setOrderBy(order);
+
+        const cardsObj = cards;
+
+        switch (order) {
+            case 'Nombre asc':
+                cardsObj.sort(compareTitleAsc);
+                return cardsObj;
+            case 'Nombre desc':
+                cardsObj.sort(compareTitleDes);
+                return cardsObj;
+            case 'Fecha asc':
+                cardsObj.sort(compareDateAsc);
+                return cardsObj;
+            case 'Fecha desc':
+                cardsObj.sort(compareDateDes);
+                return cardsObj;
+            default:
+        }
+    };
+
     const closeModal = () => {
-        setModalVisibility(false);
+        setAddCardFormVisibility(false);
+        setEditCard({});
     };
 
     return (
@@ -39,24 +99,54 @@ const Dashboard = () => {
                         <p className='message'>Ahora mismo no dispones de ninguna card</p>
                     </div>
                 ) : (
-                    <div className='main__container-cards'>
-                        {cards.map((card, id) => (
-                            <Card
-                                key={id}
-                                id={card.id}
-                                title={card.title}
-                                description={card.description}
-                                image={card.image}
-                                removeCard={() => {
-                                    removeCard(card.id);
-                                }}
+                    <Fragment>
+                        <div className='main__orderer-container'>
+                            <Orderer
+                                selectedOption={orderBy}
+                                orderOptions={[
+                                    { name: 'Nombre asc', action: setOrderCards },
+                                    { name: 'Nombre desc', action: setOrderCards },
+                                    { name: 'Fecha asc', action: setOrderCards },
+                                    { name: 'Fecha desc', action: setOrderCards },
+                                ]}
                             />
-                        ))}
-                    </div>
+                        </div>
+                        <div className='main__container-cards'>
+                            {cards.map((card, id) => (
+                                <Card
+                                    key={id}
+                                    title={card.title}
+                                    description={card.description}
+                                    image={card.image}
+                                    iconPrimary='sga-icon-times'
+                                    iconPrimaryAction={() => {
+                                        removeCard(card.id);
+                                    }}
+                                    iconSecondary='sga-icon-pencil-alt'
+                                    iconSecondaryAction={() => {
+                                        viewEditCard(card);
+                                    }}
+                                />
+                            ))}
+                        </div>
+                    </Fragment>
                 )}
             </div>
-            <RoundButton onClick={viewForm} />
-            {modalVisibility ? <FormModal closeButtonClick={closeModal} onSubmit={addCard} /> : ''}
+            <RoundButton onClick={viewAddCard} />
+            {addCardFormVisibility ? (
+                <AddCardForm closeButtonClick={closeModal} onSubmit={addCard} />
+            ) : (
+                ''
+            )}
+            {Object.keys(editCard).length > 0 ? (
+                <EditCardForm
+                    closeButtonClick={closeModal}
+                    submitChanges={saveCard}
+                    card={editCard}
+                />
+            ) : (
+                ''
+            )}
         </div>
     );
 };
